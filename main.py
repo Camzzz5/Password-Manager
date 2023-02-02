bf0e04042fe732106a0089c667769c102455464e
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-02-01 23:22:06 -0500

    set up canvas and windows

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password manager")
+canvas = Canvas(height=200, width=200)
+window.config(padx=20, pady=20)
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image = logo_image)
+canvas.pack()
+
+
+
+window.mainloop()
