1f459a234be0258e07c31db100d326ae657dbde8
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-02-02 01:02:30 -0500

    added functionality to export what the user entered to a .txt file

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save():
+    with open("data.txt", "a") as file:
+        file.write(f"{inp1.get()} | {inp3.get()} | {inp2.get()}\n")
+    inp1.delete(first=0, last=END)
+    inp2.delete(first=0, last=END)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password manager")
@@ -13,16 +17,14 @@ logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image = logo_image)
 canvas.grid(row=0, column=1)
 
-inp1 = Entry()
-inp1.config(width=35)
+inp1 = Entry(width=35)
 inp1.grid(column=1, row=1, columnspan=2)
-inp3 = Entry()
-inp3.config(width=35)
+inp3 = Entry(width=35)
 inp3.grid(column=1, row=2, columnspan=2)
-inp2 = Entry()
-inp2.config(width=21)
+inp2 = Entry(width=21)
 inp2.grid(column=1, row=3)
-
+inp1.focus()
+inp3.insert(0, "camilosantander@usantotomas.edu.co")
 LABEL1 = Label(text="Website:")
 LABEL1.grid(column=0, row=1)
 LABEL2 = Label(text="Email/Username:")
@@ -30,12 +32,12 @@ LABEL2.grid(column=0, row=2)
 LABEL3 = Label(text="Password:")
 LABEL3.grid(column=0, row=3)
 
-button1 = Button(text="Add")
+button1 = Button(text="Add", command=save)
 button1.grid(column=1, row=4, columnspan=2)
 button1.config(width=35)
 button2 = Button(text="Generate Password")
 button2.grid(column=2, row=3)
-button2.config(width=14)
+
 
 
 
